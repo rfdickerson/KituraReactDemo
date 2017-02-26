c96f14a847a3302845fc01f265a9b17ba429dd68
Robert F. Dickerson <rfdickerson@us.ibm.com>
2017-02-26 16:33:44 -0600

    Comment out the bit that doesn’t work

--- src/client/app/AwesomeComponent.jsx
+++ src/client/app/AwesomeComponent.jsx
@@ -35,6 +35,16 @@ class AwesomeComponent extends React.Component {
             });
     }
 
+    // async getMoviesFromApiAsync() {
+    //     try {
+    //         let response = await fetch('https://facebook.github.io/react-native/movies.json');
+    //         let responseJson = await response.json();
+    //         return responseJson.movies;
+    //     } catch(error) {
+    //         console.error(error);
+    //     }
+    // }
+
 }
 
 export default AwesomeComponent;
