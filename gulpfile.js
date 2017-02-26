92fa81843e9c8235311d6a99fe3358aa9af67009
Robert F. Dickerson <rfdickerson@us.ibm.com>
2017-02-26 09:37:48 -0600

    Forking a server works correctly now

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,17 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var pug = require('gulp-pug');
+
 var webpack = require('webpack-stream');
-var exec = require('gulp-exec');
+var child = require('child_process')
+
+var server = null;
 
 gulp.task('sass', function() {
     return gulp.src('./src/client/sass/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./public/css'));
 })
-
-gulp.task('html', function() {
-    return gulp.src('./src/client/templates/*.pug')
-        .pipe(pug())
-        .pipe(gulp.dest('build/html'))
-});
 
 gulp.task('sass:watch', function() {
     gulp.watch('./src/client/sass/*.scss', ['sass']);
@@ -33,18 +29,39 @@ gulp.task('develop', function() {
     gulp.watch('./src/client/sass/*.scss', ['sass']);
 });
 
-gulp.task('swift', function() {
-
-    var reportOptions = {
-        continueOnError: true,
-        pipeStdout: true
-    }
-
-    return gulp.src('./src/server/Sources/**/*.swift')
-        .pipe(exec('cd src/server && swift build'))
-        .pipe(exec('kill -3 `lsof -i :8090 | awk "FNR == 2 { print $2 }"`'))
-        .pipe(exec('cd src/server && .build/debug/Server&'))
-        .pipe(exec.reporter(reportOptions));
+gulp.task('watch', function() {
+    gulp.watch('./src/server/Sources/**/*.swift', ['swift','server']);
 });
+
+gulp.task('swift', function() {
+    return child.spawnSync('swift', ['build'], {
+        cwd: 'src/server'
+    })
+});
+
+gulp.task('server', function() {
+    if (server) 
+        server.kill();
+    server = child.spawn('./Server', [], {
+        cwd: './src/server/.build/debug'
+    });
+    server.stderr.on('data', function(data) {
+        process.stdout.write(data.toString());
+    });
+});
+
+// gulp.task('swift', function() {
+
+//     var reportOptions = {
+//         continueOnError: true,
+//         pipeStdout: true
+//     }
+
+//     return gulp.src('./src/server/Sources/**/*.swift')
+//         .pipe(exec('cd src/server && swift build'))
+//         .pipe(exec('kill -3 `lsof -i :8090 | awk "FNR == 2 { print $2 }"`'))
+//         .pipe(exec('cd src/server && .build/debug/Server&'))
+//         .pipe(exec.reporter(reportOptions));
+// });
 
 gulp.task('default', ['html', 'sass']);
